c06d67d8fdfc75e209914d7826ec921489992d9c
binhussen <mhdhsna@gmail.com>
2023-01-25 17:17:18 +0300

    Add Error Views

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { RingLoader } from 'react-spinners';
+
+const Loader = () => (
+  <div>
+    <RingLoader
+      color="#36d67a"
+      size={130}
+    />
+  </div>
+);
+
+export default Loader;
